d907c6b0e15c859b354da58251c8065664a65eb6
KimHyungSik <bagite369@naver.com>
2021-10-17 22:20:21 +0900

    update gitignore

--- app/src/main/java/com/ctu/cashstudy/CashStudyApp.kt
+++ app/src/main/java/com/ctu/cashstudy/CashStudyApp.kt
@@ -1,8 +1,16 @@
 package com.ctu.cashstudy
 
 import android.app.Application
+import com.ctu.cashstudy.core.util.CoreData.KAKAO_API_KEY
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CashStudyApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, KAKAO_API_KEY)
+    }
+
 }
---
+++ app/src/main/java/com/ctu/cashstudy/core/util/CoreData.kt
@@ -0,0 +1,5 @@
+package com.ctu.cashstudy.core.util
+
+object CoreData {
+    const val KAKAO_API_KEY = "14dfda0db277f4872199681e72a4155c"
+}
