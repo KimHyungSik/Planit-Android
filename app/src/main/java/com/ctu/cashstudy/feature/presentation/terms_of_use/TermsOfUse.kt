2f6871f6a5faad77d5e7abaac2a05b7573334080
KimHyungSik <bagite369@naver.com>
2021-10-25 00:36:11 +0900

    add login ui

---
+++ app/src/main/java/com/ctu/cashstudy/feature/presentation/terms_of_use/TermsOfUse.kt
@@ -0,0 +1,11 @@
+package com.ctu.cashstudy.feature.presentation.terms_of_use
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsOfUse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_of_use)
+    }
+}
