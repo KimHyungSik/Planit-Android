35cc4388d285c59e1fc56f42431768b59ce14466
KimHyungSik <bagite369@naver.com>
2021-11-16 04:20:47 +0900

    add appcenter dependencies

--- app/src/main/java/com/ctu/planitstudy/feature/presentation/CashStudyApp.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/presentation/CashStudyApp.kt
@@ -5,21 +5,32 @@ import android.content.Context
 import com.ctu.planitstudy.R
 import com.ctu.planitstudy.core.util.PreferencesManager
 import com.kakao.sdk.common.KakaoSdk
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class CashStudyApp:Application() {
-    companion object{
-        lateinit var prefs:PreferencesManager
+class CashStudyApp : Application() {
+    companion object {
+        lateinit var prefs: PreferencesManager
         lateinit var instance: CashStudyApp
             private set
     }
+
     override fun onCreate() {
-        prefs=PreferencesManager(applicationContext)
+        prefs = PreferencesManager(applicationContext)
         super.onCreate()
         instance = this
         KakaoSdk.init(this, getString(R.string.kakao_app_key))
+
+        AppCenter.start(
+            this, "22a02af7-2690-4035-8d51-3bb28e1f4e64",
+            Analytics::class.java, Crashes::class.java
+        )
     }
+
     fun context(): Context = applicationContext
 
 }
