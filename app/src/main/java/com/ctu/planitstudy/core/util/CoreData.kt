27dbe2e7b5abc30fb1b3229ffcebe45a02916669
KimHyungSik <bagite369@naver.com>
2021-12-01 10:03:20 +0900

    changed server url

--- app/src/main/java/com/ctu/planitstudy/core/util/CoreData.kt
+++ app/src/main/java/com/ctu/planitstudy/core/util/CoreData.kt
@@ -1,8 +1,8 @@
 package com.ctu.planitstudy.core.util
 
 object CoreData {
-//    val BASE_SERVER_URL = "https://dev-api.planit-study.com"
-    val BASE_SERVER_URL = "https://api.planit-study.com"
+    val BASE_SERVER_URL = "https://dev-api.planit-study.com"
+//    val BASE_SERVER_URL = "https://api.planit-study.com"
     val ACCESSTOKEN = "accessToken"
     val REFRESHTOKEN = "refreshToken"
     public val PREFERENCES_NAME = "PlanitStudy"
