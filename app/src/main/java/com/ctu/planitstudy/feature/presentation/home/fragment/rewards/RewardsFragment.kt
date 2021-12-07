dba82526aede9f271dbe140f889c0a4013bb6be6
KimHyungSik <bagite369@naver.com>
2021-12-07 11:10:00 +0900

    Added rotate reward

--- app/src/main/java/com/ctu/planitstudy/feature/presentation/home/fragment/rewards/RewardsFragment.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/presentation/home/fragment/rewards/RewardsFragment.kt
@@ -1,13 +1,49 @@
 package com.ctu.planitstudy.feature.presentation.home.fragment.rewards
 
 import android.view.LayoutInflater
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.activityViewModels
+import com.ctu.planitstudy.R
 import com.ctu.planitstudy.core.base.BaseFragment
 import com.ctu.planitstudy.databinding.FragmentRewardsBinding
+import com.ctu.planitstudy.feature.presentation.CashStudyApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class RewardsFragment : BaseFragment<FragmentRewardsBinding, RewardViewModel>() {
 
     override val bindingInflater: (LayoutInflater) -> FragmentRewardsBinding
         get() = FragmentRewardsBinding::inflate
     override val viewModel: RewardViewModel by activityViewModels<RewardViewModel>()
+    private val rotateAnimation = AnimationUtils.loadAnimation(
+        CashStudyApp.instance,
+        R.anim.rotate_main_reward
+    )
+
+    override fun setInit() {
+        super.setInit()
+        binding.rewardsFragmentMainRewardImg.startAnimation(
+            rotateAnimation
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(1000)
+            rotateAnimation.duration = 2000
+            binding.rewardsFragmentMainRewardImg.startAnimation(
+                rotateAnimation
+            )
+            delay(1000)
+            rotateAnimation.duration = 1000
+            binding.rewardsFragmentMainRewardImg.startAnimation(
+                rotateAnimation
+            )
+            delay(1000)
+            rotateAnimation.duration = 3000
+            binding.rewardsFragmentMainRewardImg.startAnimation(
+                rotateAnimation
+            )
+        }
+    }
 }
