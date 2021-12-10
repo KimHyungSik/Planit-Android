4cbf281d92fee1fcbaff8f4311c42ccbb9811c11
KimHyungSik <bagite369@naver.com>
2021-12-10 23:10:29 +0900

    Fixed record timer api

--- app/src/main/java/com/ctu/planitstudy/feature/data/remote/TimerApi.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/data/remote/TimerApi.kt
@@ -3,14 +3,11 @@ package com.ctu.planitstudy.feature.data.remote
 import com.ctu.planitstudy.feature.data.remote.dto.timer.TimerMeasurementDto
 import com.ctu.planitstudy.feature.data.remote.dto.util.MessageDto
 import com.ctu.planitstudy.feature.domain.model.timer.RecordMeasurementTimer
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.PUT
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface TimerApi {
 
-    @PUT("/v1/study/record/{studyId}")
+    @POST("/v1/study/record/{studyId}")
     suspend fun recordMeasurementTime(@Path("studyId") studyId: String, @Body recordMeasurementTimer: RecordMeasurementTimer): MessageDto
 
     @GET("/v1/study/record/{studyId}")
