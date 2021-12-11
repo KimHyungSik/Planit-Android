708ab35624021cf049dbc90da90a2171a3052d5b
KimHyungSik <bagite369@naver.com>
2021-12-11 23:50:45 +0900

    Added reward lottie click action

--- app/src/main/java/com/ctu/planitstudy/feature/presentation/home/fragment/rewards/RewardsFragment.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/presentation/home/fragment/rewards/RewardsFragment.kt
@@ -1,5 +1,6 @@
 package com.ctu.planitstudy.feature.presentation.home.fragment.rewards
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.activityViewModels
@@ -9,16 +10,17 @@ import com.ctu.planitstudy.databinding.FragmentRewardsBinding
 import com.ctu.planitstudy.feature.presentation.CashStudyApp
 import com.ctu.planitstudy.feature.presentation.dialogs.ReadyDialog
 import com.ctu.planitstudy.feature.presentation.util.Screens
+import kotlinx.coroutines.*
 
 class RewardsFragment : BaseFragment<FragmentRewardsBinding, RewardViewModel>() {
+
+    val TAG = "RewardsFragment - 로그"
 
     override val bindingInflater: (LayoutInflater) -> FragmentRewardsBinding
         get() = FragmentRewardsBinding::inflate
     override val viewModel: RewardViewModel by activityViewModels<RewardViewModel>()
-    private val rotateAnimation = AnimationUtils.loadAnimation(
-        CashStudyApp.instance,
-        R.anim.rotate_main_reward
-    )
+
+    private var isAnimated = false
 
     override fun setInit() {
         super.setInit()
@@ -27,15 +29,20 @@ class RewardsFragment : BaseFragment<FragmentRewardsBinding, RewardViewModel>() 
             activity = this@RewardsFragment
             viewmodel = viewModel
 
-            rewardsFragmentMainRewardImg.startAnimation(
-                rotateAnimation
-            )
-
             rewardsFragmentPlanitPassColumn.setOnClickListener {
                 moveIntent(Screens.PlanitPassScreenSh.activity)
             }
+        }
+    }
 
-            rewardsFragmentStarBg.startAnimation(RewardAnimation.getAniLeft())
+    fun touchRewardStar() {
+        if (!isAnimated) {
+            binding.rewardsFragmentMainRewardLottie.playAnimation()
+            isAnimated = true
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(binding.rewardsFragmentMainRewardLottie.duration)
+                isAnimated = false
+            }
         }
     }
 
