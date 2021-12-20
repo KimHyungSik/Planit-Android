0737bb306eb42fc7569d3fdefd4584c1eaf9f078
KimHyungSik <bagite369@naver.com>
2021-12-20 10:38:34 +0900

    Changed server uil, ktlint

--- app/src/main/java/com/ctu/planitstudy/core/util/CoreData.kt
+++ app/src/main/java/com/ctu/planitstudy/core/util/CoreData.kt
@@ -1,8 +1,8 @@
 package com.ctu.planitstudy.core.util
 
 object CoreData {
-//    val BASE_SERVER_URL = "https://dev-api.planit-study.com" // 개발 서버
-    val BASE_SERVER_URL = "https://api.planit-study.com"   // 배포 서버
+    val BASE_SERVER_URL = "https://dev-api.planit-study.com" // 개발 서버
+//    val BASE_SERVER_URL = "https://api.planit-study.com"   // 배포 서버
     val ACCESSTOKEN = "accessToken"
     val REFRESHTOKEN = "refreshToken"
     public val PREFERENCES_NAME = "PlanitStudy"
