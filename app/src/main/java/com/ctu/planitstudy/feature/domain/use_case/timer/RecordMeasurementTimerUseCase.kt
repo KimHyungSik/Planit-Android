823f972c6ad6780e51d5f5ffa25a6d0f7927c57c
KimHyungSik <bagite369@naver.com>
2021-12-26 01:08:50 +0900

    refactor: user use case

--- app/src/main/java/com/ctu/planitstudy/feature/domain/use_case/timer/RecordMeasurementTimerUseCase.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/domain/use_case/timer/RecordMeasurementTimerUseCase.kt
@@ -4,6 +4,7 @@ import com.ctu.core.util.Resource
 import com.ctu.planitstudy.feature.data.remote.dto.util.MessageDto
 import com.ctu.planitstudy.feature.domain.model.timer.RecordMeasurementTimer
 import com.ctu.planitstudy.feature.domain.repository.TimerRepository
+import com.ctu.planitstudy.feature.domain.use_case.BaseUseCase
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.json.JSONObject
@@ -12,28 +13,9 @@ import javax.inject.Inject
 
 class RecordMeasurementTimerUseCase @Inject constructor(
     private val timerRepository: TimerRepository
-) {
+) : BaseUseCase<MessageDto>() {
 
     val TAG = "RecordMeasurementTimer - 로그"
 
-    operator fun invoke(studyId: String, recordMeasurementTimer: RecordMeasurementTimer): Flow<Resource<MessageDto>> = flow {
-        try {
-            emit(Resource.Loading<MessageDto>(null))
-            val message = timerRepository.recordMeasurementTime(studyId, recordMeasurementTimer)
-            emit(Resource.Success<MessageDto>(message))
-        } catch (e: Exception) {
-            emit(Resource.Error<MessageDto>(message = "Exception" + e.message))
-            if (e is HttpException) {
-                emit(
-                    Resource.Error<MessageDto>(
-                        message = JSONObject(
-                            e.response()!!.errorBody()!!.string()
-                        ).toString()
-                    )
-                )
-            }
-        } catch (e: Throwable) {
-            emit(Resource.Error<MessageDto>(message = "Throwable" + e.message))
-        }
-    }
+    operator fun invoke(studyId: String, recordMeasurementTimer: RecordMeasurementTimer): Flow<Resource<MessageDto>> = useCase { timerRepository.recordMeasurementTime(studyId, recordMeasurementTimer) }
 }
