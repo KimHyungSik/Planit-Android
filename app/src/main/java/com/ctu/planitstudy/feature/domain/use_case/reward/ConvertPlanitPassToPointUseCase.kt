6d02fe3a9d7c8f8f85a2628de2174f5d9cfe6e9a
KimHyungSik <bagite369@naver.com>
2021-12-26 01:03:55 +0900

    refactor: reward use case

--- app/src/main/java/com/ctu/planitstudy/feature/domain/use_case/reward/ConvertPlanitPassToPointUseCase.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/domain/use_case/reward/ConvertPlanitPassToPointUseCase.kt
@@ -3,6 +3,7 @@ package com.ctu.planitstudy.feature.domain.use_case.reward
 import com.ctu.core.util.Resource
 import com.ctu.planitstudy.feature.data.remote.dto.reward.RewardDto
 import com.ctu.planitstudy.feature.domain.repository.RewardRepository
+import com.ctu.planitstudy.feature.domain.use_case.BaseUseCase
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.json.JSONObject
@@ -11,27 +12,6 @@ import javax.inject.Inject
 
 class ConvertPlanitPassToPointUseCase @Inject constructor(
     private val rewardRepository: RewardRepository
-) {
-    operator fun invoke(planetId: Int): Flow<Resource<RewardDto>> = flow {
-        try {
-            emit(Resource.Loading<RewardDto>(null))
-            val reward = rewardRepository.convertPlanetPassToPoint(planetId.toString())
-            emit(Resource.Success(reward))
-        } catch (e: NullPointerException) {
-            emit(Resource.Error<RewardDto>(message = "NullPointerException" + e.message))
-        } catch (e: Exception) {
-            emit(Resource.Error<RewardDto>(message = "Exception" + e.message))
-            if (e is HttpException) {
-                emit(
-                    Resource.Error<RewardDto>(
-                        message = JSONObject(
-                            e.response()!!.errorBody()!!.string()
-                        ).toString()
-                    )
-                )
-            }
-        } catch (e: Throwable) {
-            emit(Resource.Error<RewardDto>(message = "Throwable" + e.message))
-        }
-    }
+) : BaseUseCase<RewardDto>() {
+    operator fun invoke(planetId: Int): Flow<Resource<RewardDto>> = useCase{rewardRepository.convertPlanetPassToPoint(planetId.toString())}
 }
