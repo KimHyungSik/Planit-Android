26b144484fefa3f21ecfb6e9ac0a84de92ccdc5f
KimHyungSik <bagite369@naver.com>
2021-12-26 23:02:02 +0900

    feat: chenged google admob APPLICATION ID and Added GoogleAdmob class

---
+++ app/src/main/java/com/ctu/planitstudy/feature/data/data_source/googleadomb/GoogleAdmob.kt
@@ -0,0 +1,16 @@
+package com.ctu.planitstudy.feature.data.data_source.googleadomb
+
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.interstitial.InterstitialAd
+
+class GoogleAdmob {
+    private var mInterstitialAd: InterstitialAd? = null
+
+    init {
+        var adRequest = AdRequest.Builder().build()
+    }
+
+    fun InterstitialAdLoad(){
+
+    }
+}
