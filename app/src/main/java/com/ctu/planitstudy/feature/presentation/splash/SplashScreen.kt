c0ff886b7a4e1407f7dd66f6b8b44328ceeb97a7
KimHyungSik <bagite369@naver.com>
2021-12-31 23:27:23 +0900

    feat: internet check splash screeen

--- app/src/main/java/com/ctu/planitstudy/feature/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/presentation/splash/SplashScreen.kt
@@ -1,6 +1,13 @@
 package com.ctu.planitstudy.feature.presentation.splash
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkInfo
+import android.net.NetworkRequest
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.ctu.planitstudy.core.base.BaseBindingActivity
 import com.ctu.planitstudy.core.util.network.JWTRefreshTokenExpiration
@@ -30,33 +37,52 @@ class SplashScreen : BaseBindingActivity<ActivitySplashScreenBinding, SplashScre
     override val viewModel: SplashScreenViewModel by viewModels()
 
     val REQUEST_CODE_UPDATE = 7
+    private lateinit var cm2 : ConnectivityManager
 
-    override fun setup() {
-        val appUpdateManager = AppUpdateManagerFactory.create(CashStudyApp.instance)
-        CoroutineScope(Dispatchers.Main).launch {
-            val appUpdateInfo = appUpdateManager.appUpdateInfo.await()
-            when (appUpdateInfo.updateAvailability()) {
-                UpdateAvailability.UPDATE_AVAILABLE -> {
-                    // 업데이트 가능한 상태
-                    appUpdateManager.startUpdateFlowForResult(
-                        appUpdateInfo,
-                        AppUpdateType.IMMEDIATE,
-                        this@SplashScreen,
-                        REQUEST_CODE_UPDATE
-                    )
+    private val networkCallBack = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            // 네트워크가 연결될 때 호출됩니다.
+            val appUpdateManager = AppUpdateManagerFactory.create(CashStudyApp.instance)
+            CoroutineScope(Dispatchers.Main).launch {
+                val appUpdateInfo = appUpdateManager.appUpdateInfo.await()
+                when (appUpdateInfo.updateAvailability()) {
+                    UpdateAvailability.UPDATE_AVAILABLE -> {
+                        // 업데이트 가능한 상태
+                        appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            AppUpdateType.IMMEDIATE,
+                            this@SplashScreen,
+                            REQUEST_CODE_UPDATE
+                        )
+                    }
                 }
-            }
-            if (CashStudyApp.prefs.refreshToken != null)
-                if (CashStudyApp.prefs.refreshToken!!.isNotBlank())
-                    if (!JWTRefreshTokenExpiration().invoke()) {
-                        viewModel.getToken()
-                    } else
+                if (CashStudyApp.prefs.refreshToken != null)
+                    if (CashStudyApp.prefs.refreshToken!!.isNotBlank())
+                        if (!JWTRefreshTokenExpiration().invoke()) {
+                            viewModel.getToken()
+                        } else
+                            moveIntentAllClear(Screens.LoginScreenSh.activity)
+                    else
                         moveIntentAllClear(Screens.LoginScreenSh.activity)
                 else
                     moveIntentAllClear(Screens.LoginScreenSh.activity)
-            else
-                moveIntentAllClear(Screens.LoginScreenSh.activity)
+            }
         }
+
+        override fun onLost(network: Network) {
+
+        }
+    }
+
+    override fun setup() {
+
+        cm2 = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val builder = NetworkRequest.Builder()
+        cm2.registerNetworkCallback(
+            builder.build(),
+            networkCallBack
+        )
+
         viewModel.tokenChek.observe(
             this,
             {
