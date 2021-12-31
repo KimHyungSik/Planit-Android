db422872f2e72e8b8676570a62828ae93c4514fb
KimHyungSik <bagite369@naver.com>
2021-12-31 21:35:33 +0900

    feat: analysis toggle button with viewpager

--- app/src/main/java/com/ctu/planitstudy/feature/presentation/home/fragment/analysis/view_pager/AnalysisViewPager.kt
+++ app/src/main/java/com/ctu/planitstudy/feature/presentation/home/fragment/analysis/view_pager/AnalysisViewPager.kt
@@ -3,6 +3,7 @@ package com.ctu.planitstudy.feature.presentation.home.fragment.planner.view_page
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.ctu.planitstudy.feature.presentation.home.fragment.analysis.fragment.AnalysisAnalysisFragment
 import com.ctu.planitstudy.feature.presentation.home.fragment.analysis.fragment.AnalysisDailyReportFragment
 
 class AnalysisViewPager(fragmentActivity: FragmentActivity) :
@@ -10,6 +11,7 @@ class AnalysisViewPager(fragmentActivity: FragmentActivity) :
 
     val fragmentList = listOf<Fragment>(
         AnalysisDailyReportFragment(),
+        AnalysisAnalysisFragment()
     )
 
     override fun getItemCount(): Int = fragmentList.size
