2d3f8a0ff1f7f0c220940a49b922ea0d6b1d1c4d
KimHyungSik <bagite369@naver.com>
2021-12-31 23:34:32 +0900

    feat: added sub title dialog

---
+++ app/src/main/java/com/ctu/planitstudy/feature/presentation/dialogs/SubTitleCheckDialog.kt
@@ -0,0 +1,47 @@
+package com.ctu.planitstudy.feature.presentation.dialogs
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.ctu.planitstudy.databinding.DialogFragmentSingleTitleDdayBinding
+import com.ctu.planitstudy.feature.presentation.dday.DdayViewModel
+
+class SingleTitleCheckDialog : DialogFragment() {
+
+    val TAG = "Representative - 로그"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        isCancelable = false
+    }
+
+    private lateinit var binding: DialogFragmentSingleTitleDdayBinding
+    private val viewModel: DdayViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogFragmentSingleTitleDdayBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (arguments != null) {
+            binding.dialogTitle.text = requireArguments().getString("title")
+        }
+        binding.dDayEmptyTitleCheckConfirmed.setOnClickListener {
+            this.dismiss()
+        }
+    }
+}
