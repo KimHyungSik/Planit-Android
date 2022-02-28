40b50fb169da0bb6ac6909d5e6a5be6c85b33dc1
KimHyungSik <bagite369@naver.com>
2022-03-01 00:28:06 +0900

    Resource.kt에 http통신 코드 등록

--- app/src/main/java/com/ctu/planitstudy/core/util/Resource.kt
+++ app/src/main/java/com/ctu/planitstudy/core/util/Resource.kt
@@ -1,8 +1,8 @@
 package com.ctu.core.util
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+sealed class Resource<T>(val data: T? = null, val message: String? = null, val code: Int? = null) {
+    class Success<T>(data: T,  code: Int? = null) : Resource<T>(data, code = code)
+    class Error<T>(message: String, data: T? = null, code: Int? = null) : Resource<T>(data, message, code)
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class AppUpdate<T>() : Resource<T>(null)
 }
